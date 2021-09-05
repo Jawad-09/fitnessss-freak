74cb15bf2ce84d47ef99c565d0affac635dbcf6f
Jawad-09 <88589556+Jawad-09@users.noreply.github.com>
2021-09-05 20:06:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let saveinfoButton = document.getElementById('saveinfo');
+saveinfoButton.addEventListener('click'),function(){
+
+let name = getElementById('name');
+let age = getElementById('age');
+let sex = getElementById('sex');
+let address = getElementById('address');
+let email = getElementById('email');
+
+
+
+}
